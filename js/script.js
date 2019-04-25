14078267139349fe775ec27d0cedd9fbf5a46a5e
Prasath Ravichandran <prasath.ravichandran@acodeidea.com>
2019-04-25 11:39:36 +0530

    second commit

--- js/script.js
+++ js/script.js
@@ -4,6 +4,19 @@ $(function () {
         groupCells: true
     });
 
+    $('.image__gallery_grid').flickity({
+        cellAlign: 'left',
+        contain: true,
+        prevNextButtons: true,
+        selectedAttraction: 0.3,
+        friction: 0.8
+    });
+
+    $('.institute__inner_grid').flickity({
+        cellAlign: 'left',
+        contain: true
+    })
+
     $('.theme__block_item').click(function () {
         $('.theme__block_item.is-toggle-active').removeClass('is-toggle-active');
         $(this).addClass('is-toggle-active');
@@ -14,11 +27,13 @@ $(function () {
 
     $(window).scroll(function () {
         if ($(this).scrollTop() > 150) {
+            $('.header').addClass('active');
             $('.main_logo').addClass('active');
             $('.navbar .navbar-nav').addClass('active');
         } else {
             $('.main_logo').removeClass('active');
             $('.navbar .navbar-nav').removeClass('active');
+            $('.header').removeClass('active');
         }
     })
 });
